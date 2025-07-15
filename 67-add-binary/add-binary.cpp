1d0ab3c5742d6df810918f865e513a85b13e5f7d
Vraj Patel <vrajpatel074518@gmail.com>
2025-07-15 22:13:55 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (41.57%) - LeetSync

---
+++ 67-add-binary/add-binary.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+       int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+        string result = "";
+
+        while (i >= 0 || j >= 0 || carry) {
+            int bit1 = (i >= 0) ? a[i] - '0' : 0;
+            int bit2 = (j >= 0) ? b[j] - '0' : 0;
+
+            int sum = bit1 + bit2 + carry;
+            result += (sum % 2) + '0';
+            carry = sum / 2;
+
+            i--;
+            j--;
+        }
+
+        reverse(result.begin(), result.end());
+        return result;
+        
+    }
+};
