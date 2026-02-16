f59a2795a5972bac5a5128a886e2b472fa327ac5
Vraj Patel <vrajpatel074518@gmail.com>
2026-02-16 21:16:26 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (93.30%) - LeetSync

---
+++ 190-reverse-bits/reverse-bits.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int reverseBits(int n) {
+        int result = 0;
+        
+        for(int i = 0; i < 32; i++) {
+            result = result * 2;      // shift left
+            result = result + (n % 2); // add last bit
+            n = n / 2;               // remove last bit
+        }
+        
+        return result;
+    }
+};
