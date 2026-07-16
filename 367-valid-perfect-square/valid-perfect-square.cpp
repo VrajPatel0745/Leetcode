5da1618661a32346a00e87bfe4603afd65ca8c98
Vraj Patel <vrajpatel074518@gmail.com>
2026-07-16 21:36:03 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (53.94%) - LeetSync

---
+++ 367-valid-perfect-square/valid-perfect-square.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+         for( long int i=1;;i++)
+    {
+        if(i*i==num)
+            return true;
+        if(i*i>num)
+            return false;
+    }
+    return false;
+    }
+};
